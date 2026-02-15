b872c4370599bc4c106583c8a6f8ee7374ca00ac
Pedro <pedro3francozo@gmail.com>
2026-02-15 07:47:41 -0300

    docs: adiciona introdução e conceitos de importação

--- new_project/App.tsx
+++ new_project/App.tsx
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Olá!</Text>
-      <StatusBar style="auto" />
+      <Text> Hello!</Text>
     </View>
   );
 }
