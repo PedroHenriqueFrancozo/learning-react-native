7bba00b3b1b06d80433408b718e6dbb3f5ed36de
Pedro <pedro3francozo@gmail.com>
2026-02-15 19:41:19 -0300

    docs: corrige erro de reconhecimento de tipos de imagem (TS)

--- new_project/App.tsx
+++ new_project/App.tsx
@@ -1,5 +1,6 @@
-
 import { StyleSheet, Text, View, Image} from 'react-native';
+import symbolOn from './assets/pictures/symbol-on.png';
+import symbolOff from './assets/pictures/symbol-off.png';
 
 export default function App() {
   return (
